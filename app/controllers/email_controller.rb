a553e4d77f8a2712685bc658e2cdee048942b094
Hyeonseok <bhs0618@naver.com>
2015-06-21 14:13:01 +0000

    실제 이메일 전송 기능 탑재하기

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -6,6 +6,18 @@ end
 def email_send
 	@email = params[:email]
 	@content = params[:content]
+	@title = params[:title]
+
+gmail = Gmail.connect("draftysin@gmail.com","gustjr12")
+    gmail.deliver do
+      to email
+      subject title
+      text_part do
+        body content
+      end
+end
+
+gmail.logout
 end
 
 end
